f56c069565a11ee066ada327fd9a566c0e0d3aa3
Tom Bulled <26026015+tombulled@users.noreply.github.com>
2022-10-01 17:11:47 +0100

    Add lines between stars

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Stage, Layer, Star, Text } from 'react-konva';
+import { Stage, Layer, Star, Text, Line } from 'react-konva';
 import { useNavigate } from 'react-router-dom';
 
 function randomNumber(min, max) { 
@@ -38,27 +38,68 @@ class Home extends Component {
   render() {
     let totalStars = 5;
 
-    let maxWidth = window.innerWidth / totalStars;
+    let landscape = window.screen.orientation.type == "landscape-primary";
 
-    let starWidth = maxWidth / 4;
-    let starHeight = maxWidth / 2;
-
-    let yPadding = starHeight / 1.5;
-
+    let starsObjs = [];
     let stars = [];
+    let lines = [];
 
     for (let index = 0; index < totalStars; index++) {
-        let starX = (index * maxWidth) + (maxWidth / 2);
-        let starY = randomNumber(yPadding, window.innerHeight - yPadding);
+        var starWidth;
+        var starHeight;
+        var starX;
+        var starY;
+
+        if (landscape) {
+          let maxWidth = window.innerWidth / totalStars;
+
+          starWidth = maxWidth / 4;
+          starHeight = maxWidth / 2;
+
+          let yPadding = starHeight / 1.5;
+
+          starX = (index * maxWidth) + (maxWidth / 2);
+          starY = randomNumber(yPadding, window.innerHeight - yPadding);
+        } else {
+          let maxHeight = window.innerHeight / totalStars;
+
+          starWidth = maxHeight / 4;
+          starHeight = maxHeight / 2;
+
+          let xPadding = starWidth * 1.5;
+
+          starX = randomNumber(xPadding, window.innerWidth - xPadding);
+          starY = (index * maxHeight) + (maxHeight / 2);
+        }
 
         stars.push(
             <MapStar level={`${index + 1}`} x={starX} y={starY} width={starWidth} height={starHeight} />
         );
+
+        starsObjs.push({
+          level: index + 1,
+          x: starX,
+          y: starY,
+          width: starWidth,
+          height: starHeight
+        })
+    }
+
+    for (let index = 0; index < totalStars - 1; index ++) {
+      let starFrom = starsObjs[index];
+      let starTo = starsObjs[index + 1];
+
+      lines.push(
+        <Line
+          points={[starFrom.x, starFrom.y, starTo.x, starTo.y]}
+          stroke={"grey"}
+        />
+      )
     }
 
     return (
-      <Stage width={window.innerWidth} height={window.innerHeight}>
-        <Layer>{stars}</Layer>
+      <Stage width={window.innerWidth} height={window.innerHeight} style={{backgroundColor: "black"}}>
+        <Layer>{lines}{stars}</Layer>
       </Stage>
     );
   }
